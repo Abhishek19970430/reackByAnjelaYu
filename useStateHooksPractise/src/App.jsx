12193e6564710bdefb27a874b36ba2a0f72e0cf2
[Abhishek Verma] <[rusdaunity@gmail.com]>
2024-09-16 16:40:07 +0530

    some Update in Code

--- useStateHooksPractise/src/App.jsx
+++ useStateHooksPractise/src/App.jsx
@@ -4,19 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
-  const [TIME, setTime]= useState(0)
+  let now = new Date().toLocaleTimeString();
+  const [time, setTime]= useState(now)
   
   function currentTime(){
-    let time = new Date().toLocaleTimeString();
-    setTime(time);
+    let newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
     
   }
   setInterval(currentTime, 1000);
   
     return (
       <div className="container">
-        <h1>{TIME}</h1>
+        <h1>{time}</h1>
         <button onClick={currentTime}>Get Time</button>
       </div>
     );
